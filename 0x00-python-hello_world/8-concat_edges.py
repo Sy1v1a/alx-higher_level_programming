e929c10ef6e0134102a6f4e0ec9069a036ec7094
sy1v1a <oyinyechi@ymail.com>
2023-06-05 04:31:40 -0700

    all

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+ separate = str.split(' ')
+print(separate[5:7] + str[12] + str[0])
