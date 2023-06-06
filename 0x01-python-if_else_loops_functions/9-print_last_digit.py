f5d15b1e0fb4115e5fb66f0c594571fd96a71e67
sy1v1a <oyinyechi@ymail.com>
2023-06-06 09:51:52 -0700

    l

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#this is just to let you know that we needed 88 characters so this ...
 def print_last_digit(number):
     last = number % 10
     print("{}".format(last), end='')
