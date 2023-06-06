704dd1e33ecdda4bdd700349220bd376fb2e1c33
sy1v1a <oyinyechi@ymail.com>
2023-06-06 10:17:36 -0700

    all

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-#this is just to let you know that we needed 88 characters so this ...
+"""a function to print last disit"""
 def print_last_digit(number):
     last = number % 10
     print("{}".format(last), end='')
-    return last
+    return (last)
