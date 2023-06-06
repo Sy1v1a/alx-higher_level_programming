765178f3f45397f6c51a802818bd479406140c4d
sy1v1a <oyinyechi@ymail.com>
2023-06-06 06:04:35 -0700

    upper

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(9):
+    for j in range(10):
+        if i == j:
+            continue
+        else:
+            print(f"{i}{j}", end=', ')
+print()
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letters in str:
+            upperc = chr(ord(letters) - 32) if ord(letters) >= 97 and ord(letters) <= 122 else letters
+            print(upperc, end='')
+    print()
