a7b5c21dd15c9dcf423404af24010622e001b034
sy1v1a <oyinyechi@ymail.com>
2023-06-08 06:45:14 -0700

    ll

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+str = chr(ord('A')), chr(ord('Z'))
+print(str)
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
-ag = sys.argv[1:]
-sum1 = 0
-for i in ag:
-    sum1 += int(i)
-    print("{}".format(sum1))
+if __name__ == "__main__":
+    ag = sys.argv[1:]
+    sum1 = 0
+    for i in ag:
+        sum1 += int(i)
+        print("{}".format(sum1))
