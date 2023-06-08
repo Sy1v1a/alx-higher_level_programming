6ace4ee0a92d57a61b120a7c2bc480ce8f32a74b
sy1v1a <oyinyechi@ymail.com>
2023-06-07 17:02:49 -0700

    aba

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 """a function to print last disit"""
+
 def print_last_digit(number):
-    last = number % 10
+    last = abs(number) % 10
     print("{}".format(last), end='')
     return (last)
