24f7a79cbe90b798443481d8ab8002a31238b18b
sy1v1a <oyinyechi@ymail.com>
2023-06-11 22:59:17 -0700

    all

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,4 +4,3 @@ def element_at(my_list, idx):
         return (none)
     else:
         return (my_list[idx])
-
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,5 +5,5 @@ def max_integer(my_list=[]):
     maxi = my_list[0]
     for i in range(len(my_list)):
         if my_list[i] > maxi:
-            maxi = my_list[ i]
+            maxi = my_list[i]
     return (maxi)
