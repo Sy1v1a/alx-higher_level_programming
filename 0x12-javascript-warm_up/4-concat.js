71461dcec69543fed414b4df9a9260a7fa21dbe7
Sylvia Ogbonna <125444305+Sy1v1a@users.noreply.github.com>
2023-09-12 00:42:25 +0100

    Update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
+//const args = process.argv.slice(2);
 
+//if (args.length === 2) {
+//  console.log(args[0] 'is' args[1]);
+//} else if (args.length === 1) {
+//  console.log(args[0] 'is undefined');
+//} else {
+ // console.log('undefined is undefined');
+//}
 const args = process.argv.slice(2);
 
 if (args.length === 2) {
-  console.log(args[0] 'is' args[1]);
-} else if (args.length === 1) {
-  console.log(args[0] 'is undefined');
+  console.log(args[0] + ' is ' + args[1]);
 } else {
   console.log('undefined is undefined');
 }
