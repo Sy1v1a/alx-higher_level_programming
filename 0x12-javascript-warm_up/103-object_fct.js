39b9000d7c661b9dc3c966e0d94a8a604230b678
Sylvia Ogbonna <125444305+Sy1v1a@users.noreply.github.com>
2023-09-12 00:50:50 +0100

    Update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,17 +1,20 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
-  incr: function () {
-    this.value++;
-  }
+  value: 12
 };
+
+const incr = () => {
+  myObject.value += 1;
+};
+
+console.log(myObject);
 
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
-myObject.incr();
-console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
