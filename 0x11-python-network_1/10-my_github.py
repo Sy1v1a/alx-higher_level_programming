cd809c8a9f254fca4b642c03987b7127e8b0f29c
Sylvia Ogbonna <125444305+Sy1v1a@users.noreply.github.com>
2023-09-29 13:12:31 +0100

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,9 @@
+
 #!/usr/bin/python3
-""" takes your GitHub credentials (username and password) and uses the GitHub API to display your id"""
+"""
+takes your GitHub credentials (username and password) and 
+uses the GitHub API to display your id
+"""
 import requests
 import sys
 from requests.auth import HTTPBasicAuth
