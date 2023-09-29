6e7b8cadbdc558054ea4489ed9c363e016c0fc04
Sylvia Ogbonna <125444305+Sy1v1a@users.noreply.github.com>
2023-09-29 12:35:50 +0100

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,21 @@
-#
+#!/usr/bin/python3
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python 5-hbtn_header.py <URL>")
+    sys.exit(1)
+
+url = sys.argv[1]
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+
+    x_request_id = response.headers.get('X-Request-Id')
+    if x_request_id:
+        print(x_request_id)
+    else:
+        print("X-Request-Id header not found in the response.")
+except requests.exceptions.RequestException as e:
+    print("Error fetching the URL:", e)
