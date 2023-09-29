ea96726c9647735a11e295de28cdf5a703447ded
Sylvia Ogbonna <125444305+Sy1v1a@users.noreply.github.com>
2023-09-29 12:37:54 +0100

    Create 6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: python 6-post_email.py <URL> <email>")
+    sys.exit(1)
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = {'email': email}
+
+try:
+    response = requests.post(url, data=data)
+    response.raise_for_status()  # Raise an exception for HTTP errors (4xx and 5xx status codes)
+
+    print("Your email is:", response.text)
+except requests.exceptions.RequestException as e:
+    print("Error sending POST request:", e)
