d8fe8dc868d00a75fc39c6d337370e1b27c9f249
Sylvia Ogbonna <125444305+Sy1v1a@users.noreply.github.com>
2023-09-29 12:33:31 +0100

    Create 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import requests
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
+except requests.exceptions.RequestException as e:
+    print("Error fetching the URL:", e)
