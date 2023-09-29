2aca6d37b48bf692335b86fd2dd5327e2937af34
Sylvia Ogbonna <125444305+Sy1v1a@users.noreply.github.com>
2023-09-29 12:45:46 +0100

    Create 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: python 10-my_github.py <username> <personal_access_token>")
+    sys.exit(1)
+
+username = sys.argv[1]
+access_token = sys.argv[2]
+
+url = f"https://api.github.com/sy1v1a"
+
+# Create a Basic Authentication header with your username and personal access token
+headers = {
+    'Authorization': f'Basic {username}:{access_token}'
+}
+
+try:
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    user_data = response.json()
+    if 'id' in user_data:
+        print(user_data['id'])
+    else:
+        print("Unable to retrieve user ID.")
+except requests.exceptions.RequestException as e:
+    print("Error accessing GitHub API:", e)
