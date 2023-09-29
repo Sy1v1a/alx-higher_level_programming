7d4c0880d69b946b9a6586bee1e116797bf4f0c0
Sylvia Ogbonna <125444305+Sy1v1a@users.noreply.github.com>
2023-09-29 12:49:59 +0100

    Create 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: python 100-github_commits.py <repository name> <owner name>")
+    sys.exit(1)
+
+repository_name = sys.argv[1]
+owner_name = sys.argv[2]
+
+url = f"https://api.github.com/repos/{owner_name}/{repository_name}/commits"
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for HTTP errors (4xx and 5xx status codes)
+
+    commits = response.json()
+    
+    # Print the 10 most recent commits
+    for commit in commits[:10]:
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{sha}: {author_name}")
+except requests.exceptions.RequestException as e:
+    print("Error accessing GitHub API:", e)
